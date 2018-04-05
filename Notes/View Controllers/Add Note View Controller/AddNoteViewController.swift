7a71b4a53ab8b1f0d9fcdf925b9d3f58775d74c2
PinguMac <>
2018-04-05 10:12:46 +0800

    Add a view controller to manage save operation

---
+++ Notes/View Controllers/Add Note View Controller/AddNoteViewController.swift
@@ -0,0 +1,22 @@
+//
+//  AddNoteViewController.swift
+//  Notes
+//
+//  Created by PinguMac on 2018/4/5.
+//  Copyright © 2018年 PinguMac. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class AddNoteViewController: UIViewController {
+  
+  // MARK: - Properties
+  @IBOutlet weak var titleTextField: UITextField!
+  @IBOutlet weak var contentsTextView: UITextView!
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+  
+}
