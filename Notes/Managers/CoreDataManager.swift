c66727f2a417a56a15cd2aad61256da79647f701
PinguMac <>
2018-04-05 13:23:09 +0800

    Save managed object context when the application is about to enter the background and before it's about to be terminated by the operating system.

--- Notes/Managers/CoreDataManager.swift
+++ Notes/Managers/CoreDataManager.swift
@@ -65,5 +65,33 @@ final class CoreDataManager {
   // MARK: - Initialization
   init(modelName: String) {
     self.modelName = modelName
+    
+    setupNotificationHandling()
+  }
+  
+  // MARK: - Helper Methods
+  private func setupNotificationHandling() {
+    let notificationCenter = NotificationCenter.default
+    notificationCenter.addObserver(self,
+                                   selector: #selector(saveChanges(_:)),
+                                   name: Notification.Name.UIApplicationWillTerminate,
+                                   object: nil)
+    notificationCenter.addObserver(self,
+                                   selector: #selector(saveChanges(_:)),
+                                   name: Notification.Name.UIApplicationDidEnterBackground,
+                                   object: nil)
+  }
+  
+  // MARK: -
+  @objc private func saveChanges(_ notification: Notification) {
+    guard managedObjectContext.hasChanges else { return }
+    
+    do {
+      try managedObjectContext.save()
+    } catch {
+      print("Unable to Save Managed Object Context")
+      print("\(error), \(error.localizedDescription)")
+    }
+    
   }
 }
