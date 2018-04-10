0feb7f7a072a1e00e5159fd456850e721c5a6d5e
PinguMac <>
2018-04-10 09:48:19 +0800

    Implement AddCatrgoryViewController

--- Notes/View Controllers/Categories/Add Category View Controller/AddCategoryViewController.swift
+++ Notes/View Controllers/Categories/Add Category View Controller/AddCategoryViewController.swift
@@ -11,12 +11,55 @@ import CoreData
 
 class AddCategoryViewController: UIViewController {
   
+  // MARK: - Properties
+  @IBOutlet weak var nameTextField: UITextField!
   
-  // MARK: -
+  // MARK: - View Life Cycle
   var managedObjectContext: NSManagedObjectContext?
   
   override func viewDidLoad() {
     super.viewDidLoad()
     
+    title = "Add Category"
+    
+    setupView()
+  }
+  
+  override func viewDidAppear(_ animated: Bool) {
+    super.viewDidAppear(animated)
+    
+    // Show Keyboard
+    nameTextField.becomeFirstResponder()
+  }
+  
+  // MARK: - View Methods
+  private func setupView() {
+    setupBarButtonItems()
+  }
+  
+  // MARK: -
+  private func setupBarButtonItems() {
+    navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .save, target: self, action: #selector(save(sender:)))
+  }
+  
+  // MARK: - Actions
+  @IBAction private func save(sender: UIBarButtonItem) {
+    guard let managedObjectContext = managedObjectContext else {
+      return
+    }
+    
+    guard let name = nameTextField.text, !name.isEmpty else {
+      showAlert(with: "Name Missing", and: "Your category doesn't have a name.")
+      return
+    }
+    
+    // Create Category
+    let category = Category(context: managedObjectContext)
+    
+    // Configure Category
+    category.name = nameTextField.text
+    
+    // Pop View Controller
+    _ = navigationController?.popViewController(animated: true)
   }
 }
